1ab60f9c47df182193b2d55dd4e26c265d2e8277
piltirmon86 <fernandogomezrecio@gmail.com>
2023-08-14 18:10:58 +0200

    user routes perfil de usuario

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,13 +2,19 @@
 const express = require('express');
 const { isLoggedIn } = require('../middlewares/route-guard');
 const router = express.Router();
+const User = require('../models/User.model'); //nuevo
+
+//User Profile
 
 router.get("/user/user-profile", isLoggedIn, (req, res) => {
-    // res.send("hola")
-    res.render("user/user-profile")
 
+    const userId = req.session.currentUser
+
+    User
+        .findById(userId)
+        .then(user => res.render("user/user-profile", user))
+        .catch(err => console.log(err))
 })
-
 
 router.post("/user/user-profile", isLoggedIn, (req, res) => {
     res.render("user/user-profile")
